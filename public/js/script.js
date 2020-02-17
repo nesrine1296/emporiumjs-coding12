d97d762c84a4cfdca4cbfe1582246e2f215ffe27
Nesrine <bouhejbanesrine@gmail.com>
2020-02-17 09:47:34 +0100

    footer terminé

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+let changecolor = document.getElementById (button)[0]
+let body = document.querySelector ('body')
+body.style.backgroundColor = 'white'
+
+
+
+
+changecolor.addEventListener ('click',() => {
+
+    body.style.backgroundColor = 'black'
+    
+})
+    
+    
